4d4efba4eeb9e562fcc0beaa0d576bc351b4b462
Migle DB <dubauskaitemigle@gmail.com>
2025-10-16 19:44:12 +0200

    FR y FW ejercicio con numeros

--- src/fpcore25_26_AD/ficheros_archivos/ejParesImpares/Numeros.java
+++ src/fpcore25_26_AD/ficheros_archivos/ejParesImpares/Numeros.java
@@ -1,29 +1,56 @@
 package fpcore25_26_AD.ficheros_archivos.ejParesImpares;
 
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Numeros {
-	
+
 	public static void main(String[] args) {
-		
-		int[] numerosPares = {1};
-		
-		try(FileWriter fw = new FileWriter("src/ficheros/pares.txt")){
+
+		List<Integer> numerosPares = new ArrayList<>();
+		List<Integer> numerosImpares = new ArrayList<>();
+
+		try (FileWriter fw1 = new FileWriter("src/ficheros/numerosPares.txt");
+				FileWriter fw2 = new FileWriter("src/ficheros/numerosImpares.txt");) {
 			
-			for (int i = 0; i < 10; i++) {
-				fw.append((char)i);
-				if (i != numerosPares.length - 1)
-					fw.append("\n");
+			for (int i = 0; i < 20; i++) {
+				if(i % 2 == 0) fw1.write(i + "\n");
+				else fw2.write(i + "\n");
+				
 			}
-			fw.flush();
-			System.out.println(numerosPares);
-			
+			fw1.flush();
+			System.out.println("Numeros agregados a los archivos");
+
 		} catch (IOException e) {
-			// TODO: handle exception
+			System.out.println(e.getMessage());
+		}
+		finally {
+			System.out.println("Gracias por confiar en nosotros!");
 		}
 		
+		try (FileReader fr1 = new FileReader("src/ficheros/numerosPares.txt");
+				FileReader fr2 = new FileReader("src/ficheros/numerosImpares.txt");
+				FileWriter frTodos = new FileWriter("src/ficheros/todosNumeros.txt");
+				FileWriter frOrdenados = new FileWriter("src/ficheros/numerosOrdenados.txt");
+				) {
+			
+			int c; 
+			while((c = fr1.read()) != -1) {
+				frTodos.write(c);
+			}
+			while((c = fr2.read()) != -1) {
+				frTodos.write(c);
+			}
+			System.out.println();
+			
+		} catch (IOException e) {
+			System.out.println(e.getMessage());
+		}
 		
 	}
+	
 
 }
